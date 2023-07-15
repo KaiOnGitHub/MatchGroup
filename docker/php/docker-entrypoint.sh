2f38f1aaa7998b1f94f89ba642848cd550d97551
Kai Koenig <kai.koenig@pm.me>
2023-07-15 22:35:24 +0200

    assetmapper build step

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -26,6 +26,9 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 	if [ "$APP_ENV" != 'prod' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
+
+		# Install assets from asset mapper
+		php bin/console asset-map:compile
 	fi
 
 	if grep -q ^DATABASE_URL= .env; then
