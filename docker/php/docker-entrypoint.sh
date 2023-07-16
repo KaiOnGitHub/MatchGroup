0cb07b51c6112a2b51da9881999c9aaf0bcc0f4a
Kai Koenig <kai.koenig@pm.me>
2023-07-16 13:32:23 +0200

    dont install the assets again, we just deploy them

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -28,12 +28,6 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
 
-	if [ "$APP_ENV" = 'prod' ]; then
-		# Install assets from asset mapper
-        echo "Installing assets from asset mapper..."
-        php bin/console asset-map:compile
-	fi
-
 	if grep -q ^DATABASE_URL= .env; then
 		# After the installation, the following block can be deleted
 		if [ "$CREATION" = "1" ]; then
